5d1a1716ea96ef6666458268c19436f652e0936c
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-10-09 10:31:43 -0400

    1

---
+++ 6 kyu/counting-duplicates.js
@@ -0,0 +1,6 @@
+function duplicateCount(text){
+ 
+    return text.toLowerCase().split('').filter((val, i, arr) => {
+      return !!(arr.indexOf(val) !== i && arr.lastIndexOf(val) === i);
+    }).length;
+  }
