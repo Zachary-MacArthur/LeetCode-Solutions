19136481e05452b2146a44122c9224a51ef560b4
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-11-07 18:11:39 -0500

    easy

---
+++ easy/fibinacci-problem.js
@@ -0,0 +1,24 @@
+//ex
+//input n = 4 
+//output: 3
+//ex: f(4) = f(3) + f(2) = 2 + 1 = 3
+//given n calculate F(n)
+
+//fib --> 0, 1, 1, 2, 3
+//fib --> num > that many nums in the fib sequence 
+//return: last num in sequence
+//ex
+// 0, 1, 1, 2, 3, 5, 8, 13
+//F(3) -> 2 
+//F(4) -> 3
+//console.log( fib(3), '2')
+
+function fib(numsGenerated){
+    let arr = [0,1]
+    for (let i = 2; i <= numsGenerated; i++ ){
+        let a = arr[i - 1]
+        let b = arr[i -2]
+        arr.push(a + b)
+    }
+    return sequence[numsGenerated]
+}
