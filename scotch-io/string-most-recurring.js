f4ce48be59225380b25307219342d659e43f1b45
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-11-20 13:45:40 -0500

    max char

---
+++ scotch-io/string-most-recurring.js
@@ -0,0 +1,22 @@
+
+function maxRecurringChar(text){
+    let charMap = {}
+    let maxCharValue = 0
+    let maxChar = ''
+
+    for (let char of text) {
+        if (charMap.hasOwnProperty(char)) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > maxCharValue) {
+            maxCharValue = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
