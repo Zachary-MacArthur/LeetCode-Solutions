b912debe2800ff2647f054cbc891d16e8bd71b3f
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-11-23 09:53:03 -0500

    scotch

--- scotch-io/capitalize.js
+++ scotch-io/capitalize.js
@@ -21,3 +21,15 @@ function capSentence(text) {
     })
     return capsArray.join(' ')
 }
+
+//using replace()
+
+function capSentence(text) {
+    let wordsArray = text.toLowerCase().split(' ')
+
+    let capsArray = wordsArray.map( word=>{
+        return  word.replace(word[0], word[0].toUpperCase())
+    })
+
+    return capsArray.join(' ')
+    }
