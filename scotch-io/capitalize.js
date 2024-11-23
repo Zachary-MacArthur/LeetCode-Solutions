8361451e2dade8fe7ee7d46e798be98f406aa7a4
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-11-23 09:51:29 -0500

    scotch

---
+++ scotch-io/capitalize.js
@@ -0,0 +1,23 @@
+// Given a sentence containing two or more words, return the equivalent of the sentence when capitalized. E.g
+// capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!'
+
+function capSentence(text) {
+    let wordsArray = text.toLowerCase().split(' ')
+    let capsArray = []
+
+    wordsArray.forEach(word => {
+        capsArray.push(word[0].toUpperCase() + word.slice(1))
+    });
+
+    return capsArray.join(' ')
+}
+
+//or with map
+
+function capSentence(text) {
+    let wordsArray = text.toLowerCase().split(' ')
+    let capsArray = wordsArray.map(word=>{
+        return word[0].toUpperCase() + word.slice(1)
+    })
+    return capsArray.join(' ')
+}
