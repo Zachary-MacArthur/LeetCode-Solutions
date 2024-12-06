c5f7d6d9e7dd0e309482e6ad1b861f630fe022a5
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2024-12-06 14:10:25 -0500

    scotch

---
+++ where-do-i-belong.js
@@ -0,0 +1,25 @@
+//Return the lowest index at which a value (second argument) i.e **num** should be inserted into an array (first argument) i.e **arr** once it has been sorted. The returned value should be a number
+
+//whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater than 1(index 0), but less than 2(index 1).
+
+//for loop
+
+function wherIBelong(arr, num){
+    arr.sort((a,b) => {
+        return a - b
+    })
+    for (var i= 0; i < arr.length; i++) {
+        if (arr[i] >= num) {
+            return i
+        }
+    }
+    return arr.length
+}
+
+//or 
+function wherIBelong(arr, num) {
+    arr.push(num)
+    arr.sort((a, b) => a - b)
+    return arr.endexOf(num)
+}
+
